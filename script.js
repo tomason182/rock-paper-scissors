3aad87a8e5322403cdbec095c775b588cec697c8
tomason182 <elotro182@hotmail.com>
2024-02-16 18:21:50 -0300

    modified array selection

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 let playerChoice = prompt("Select Rock, Paper or Scissors: ").toLowerCase();
 let computerChoice = getComputerChoice();
 
+function getplayerChoice(playerChoice) {
+        if (playerChoice === "rock") {
+            let playerSelection = 0;
+        }else if (playerChoice === "paper") {
+            let playerSelection = 1;
+        }else if (playerChoice === "scissors") {
+            let playerSelection = 2;
+        }else {
+            let playerSelection = prompt("Wrong Selection. Select Rock, Paper or Scissors: ")
+        }
+        return playerSelection
+}
+
 function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
-
-    let randomChoice = choices[randomNumber()];
-    return randomChoice
-
-}
-
-function randomNumber() {
-    return Math.floor(Math.random() * 3)
+    computerSelection = Math.floor(Math.random() * 3)
+    return computerSelection
 }
 
 
-
-function playRound() {
+function playRound(playerSelection, computerSelection) {
     const gameArray =[[-1,0,1],[1,-1,0],[0,1,-1]];
-    return gameArray[2][0]
+    return gameArray[playerSelection][computerSelection]
     
 
 }
