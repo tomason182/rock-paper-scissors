6d4272d1d35304eef1fef69c4a48c2b528c3da25
tomason182 <elotro182@hotmail.com>
2024-02-17 10:36:45 -0300

    final result, add comments

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-//Player select a choice
+//Ask user for selection. Store it
 let playerChoice = prompt("Select Rock, Paper or Scissors: ").toLowerCase();
-//Computer select a choice (number)
+
+//Ask computer for selection. Store it
 let computerChoice = getComputerChoice();
 
-<<<<<<< HEAD
+//modify player selection to intiger to compare
 let userChoice = playerChoiceToInt(playerChoice)
 
-//Transform player choice into integer
+//Transform player choice into integer function
 function playerChoiceToInt(playerChoice) {
         let playerSelection
 
@@ -19,31 +20,11 @@ function playerChoiceToInt(playerChoice) {
         }else {
             playerSelection = alert("Wrong Selection. Select Rock, Paper or Scissors.")
         }
-        return playerSelection
-=======
-function playRound(getplayerChoice, computerChoice) {
-    const gameArray = [[-1, 0, 1], [1, -1, 0], [0, 1, -1]];
-    let result = gameArray[getplayerChoice][computerChoice];
-    return result;
-}
+        return playerSelection}
 
-function getplayerChoice(playerChoice) {
-    let playerSelection;
-
-    if (playerChoice === "rock") {
-        playerSelection = 0;
-    } else if (playerChoice === "paper") {
-        playerSelection = 1;
-    } else {
-        playerSelection = 2; // Cambiado de playerChoice a playerSelection
-    }
-    return playerSelection;
->>>>>>> origin
-}
-
-//random number between 0 and 2
+//random number between 0 and 2 function
 function getComputerChoice() {
-<<<<<<< HEAD
+
     let computerSelection = Math.floor(Math.random() * 3);
     return computerSelection
 }
@@ -78,40 +59,9 @@ function finalResult(playRoundResult, playerChoice, computerChoiceToWords) {
     }else if (playRoundResult === 0) {
         finalResult = `You Loose! ${computerChoiceToWords} beats ${playerChoice} `;
     }else {
-        finalResult = `Yoy Win! ${playerChoice} beats ${computerChoiceToWords}`
-=======
-    computerSelection = Math.floor(Math.random() * 3);
-    return computerSelection;
+        finalResult = `Yoy Win! ${playerChoice} beats ${computerChoiceToWords}`}
+    return finalResult
 }
 
-function computerToWords(computerChoice) {
-    let var2;
-    if (computerChoice === 0) {
-        var2 = "rock";
-    } else if (computerChoice === 1) {
-        var2 = "paper";
-    } else {
-        var2 = "scissors"; // Cambiado de "scissors" a var2
-    }
-    return var2;
-}
-
-function finalResult(result) {
-    let finalResult;
-    if (result === -1) {
-        finalResult = `You tied! ${playerChoice} tieds with ${computerToWords(computerChoice)}`;
-    } else if (result === 0) {
-        finalResult = `You Loose! ${computerToWords(computerChoice)} beats ${playerChoice} `;
-    } else {
-        finalResult = `You Win! ${playerChoice} defeats ${computerToWords(computerChoice)}`;
->>>>>>> origin
-    }
-    return finalResult;
-}
-
-<<<<<<< HEAD
-
+//Show result in console.log
 console.log(finalResult(playRoundResult, playerChoice, computerChoiceToWords(computerChoice)))
-=======
-console.log(finalResult(playRound(getplayerChoice(playerChoice), computerChoice))); // Pasando los argumentos necesarios
->>>>>>> origin
