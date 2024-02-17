30de3f88c2a2e300d03e35d3e8487bcbc8ace277
tomason182 <elotro182@hotmail.com>
2024-02-17 10:16:01 -0300

    not finale result

--- script.js
+++ script.js
@@ -1,54 +1,52 @@
 let playerChoice = prompt("Select Rock, Paper or Scissors: ").toLowerCase();
 let computerChoice = getComputerChoice();
 
-function getplayerChoice(var1) {
-        let playerSelection;
+function playRound(getplayerChoice, computerChoice) {
+    const gameArray = [[-1, 0, 1], [1, -1, 0], [0, 1, -1]];
+    let result = gameArray[getplayerChoice][computerChoice];
+    return result;
+}
 
-        if (var1 === "rock") {
-            playerSelection = 0;
-        }else if (var1 === "paper") {
-            playerSelection = 1;
-        }else {
-            var1 = 2;     
-        }
-        return playerSelection
+function getplayerChoice(playerChoice) {
+    let playerSelection;
+
+    if (playerChoice === "rock") {
+        playerSelection = 0;
+    } else if (playerChoice === "paper") {
+        playerSelection = 1;
+    } else {
+        playerSelection = 2; // Cambiado de playerChoice a playerSelection
+    }
+    return playerSelection;
 }
 
 function getComputerChoice() {
     computerSelection = Math.floor(Math.random() * 3);
-    return computerSelection
+    return computerSelection;
 }
 
 function computerToWords(computerChoice) {
-    if (computerChoice = 0) {
+    let var2;
+    if (computerChoice === 0) {
         var2 = "rock";
-    }else if (computerChoice = 1) {
+    } else if (computerChoice === 1) {
         var2 = "paper";
-    }else {var2 = "scissors"}
-
+    } else {
+        var2 = "scissors"; // Cambiado de "scissors" a var2
+    }
     return var2;
 }
-
-function playRound(getplayerChoice, computerChoice) {
-    const gameArray =[[-1,0,1],[1,-1,0],[0,1,-1]];
-    let result = gameArray[getplayerChoice][computerChoice];
-    return result
-}
-
 
 function finalResult(result) {
     let finalResult;
     if (result === -1) {
-        finalResult = `You tied! ${playerChoice} tieds with ${computerToWords()}`;
-    }else if (result === 0) {
-        finalResult = `You Loose! ${computerToWords} beats ${playerChoice} `;
-    }else {
-        finalResult = `Yoy Win! ${playerChoice} defeat ${computerToWords()}`;
+        finalResult = `You tied! ${playerChoice} tieds with ${computerToWords(computerChoice)}`;
+    } else if (result === 0) {
+        finalResult = `You Loose! ${computerToWords(computerChoice)} beats ${playerChoice} `;
+    } else {
+        finalResult = `You Win! ${playerChoice} defeats ${computerToWords(computerChoice)}`;
     }
-    return finalResult
+    return finalResult;
 }
 
-
-console.log(computerToWords(computerChoice))
-console.log(getplayerChoice(playerChoice))
-console.log(finalResult())
+console.log(finalResult(playRound(getplayerChoice(playerChoice), computerChoice))); // Pasando los argumentos necesarios
