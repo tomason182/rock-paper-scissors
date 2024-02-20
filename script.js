ce6cdefa1cd7ebc3f9d5f0e544529d49d666cab8
tomason182 <elotro182@hotmail.com>
2024-02-20 13:49:06 -0300

    querySelector button function

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 //Ask user for selection. Store it
-let playerChoice = document.querySelectorAll('button');
+let buttonSelection = document.querySelectorAll('button');
 
-playerChoice.forEach((choice) => {
-    choice.addEventListener('click', () => {alert(choice.id);
-    })
-})
+buttonSelection.forEach((button) => {
+    button.addEventListener('click', () => {
+    playerChoice = parseInt(button.id);
+    console.log(playerChoice);
+    });
+});
+
+let nweVar = playerChoice
+console.log('the other', nweVar)
 
 //Ask computer for selection. Store it
-let computerChoice = getComputerChoice();
+//let computerChoice = getComputerChoice();
 
 //modify player selection to intiger to compare
-let userChoice = playerChoiceToInt(playerChoice)
+//let userChoice = playerChoiceToInt(playerChoice)
 
 //Transform player choice into integer function
-function playerChoiceToInt(playerChoice) {
-        let playerSelection
+//function playerChoiceToInt(playerChoice) {
+//       let playerSelection
 
-        if (playerChoice === "rock") {
+        /* if (playerChoice === "rock") {
              playerSelection = 0;
         }else if (playerChoice === "paper") {
             playerSelection = 1;
@@ -26,18 +31,18 @@ function playerChoiceToInt(playerChoice) {
             playerSelection = alert("Wrong Selection. Select Rock, Paper or Scissors.")
         }
         return playerSelection}
-
+ */
 //random number between 0 and 2 function
-function getComputerChoice() {
+//function getComputerChoice() {
 
-    let computerSelection = Math.floor(Math.random() * 3);
+  /*   let computerSelection = Math.floor(Math.random() * 3);
     return computerSelection
 }
-
+ */
 //convert computer choice to Rock, Paper or Scissors
 
-function computerChoiceToWords(computerChoice) {
-    let computerToWords;
+//function computerChoiceToWords(computerChoice) {
+/*     let computerToWords;
     if (computerChoice === 0) {
         computerToWords = "Rock";
     }else if (computerChoice === 1) {
@@ -46,16 +51,16 @@ function computerChoiceToWords(computerChoice) {
 
     return computerToWords
 }
-
+ */
 //Game logic
 
-function playRound(userChoice, computerChoice) {
+/* function playRound(userChoice, computerChoice) {
     const gameArray =[[-1,0,1],[1,-1,0],[0,1,-1]];
     let result = gameArray[userChoice][computerChoice];
     return result
-}
+} */
 
-let playRoundResult = playRound(userChoice, computerChoice)
+/* let playRoundResult = playRound(userChoice, computerChoice)
 
 function finalResult(playRoundResult, playerChoice, computerChoiceToWords) {
     let finalResult
@@ -69,4 +74,4 @@ function finalResult(playRoundResult, playerChoice, computerChoiceToWords) {
 }
 
 //Show result in console.log
-console.log(finalResult(playRoundResult, playerChoice, computerChoiceToWords(computerChoice)))
+console.log(finalResult(playRoundResult, playerChoice, computerChoiceToWords(computerChoice))) */
