5d0efc68151ac312c95209f6ef31e05a3788303a
tomason182 <elotro182@hotmail.com>
2024-02-20 19:43:29 -0300

    not getting the loop done. ERROR

--- script.js
+++ script.js
@@ -1,81 +1,74 @@
-function handleButtonClick(button) {
-    let playerChoice = parseInt(button.id);
-    let computerChoice = getComputerChoice();
-    let playRoundResult = playRound(playerChoice, computerChoice);
+let playerScore = 0;
+let computerScore = 0;
 
-    console.log("Player Choice:", playerChoice);
-    console.log("Computer Choice:", computerChoice);
-    console.log("Play Round Result: ", playRoundResult);
+while(playerScore < 5 && computerScore <5) {
+
+    function handleButtonClick(button) {        
+
+            let playerChoice = parseInt(button.id);
+            let computerChoice = getComputerChoice();
+            let playRoundResult = playRound(playerChoice, computerChoice);
+
+            if (playRoundResult === 1) {
+                playerScore += 1;
+            }else if (playRoundResult === 0) {
+                computerScore += 1;
+            }
+                    
+            let playerChoiceText = playerChoiceToWords(playerChoice);
+            let computerChoiceText = computerChoiceToWords(computerChoice); 
+
+            const result = document.querySelector('#final-result');
+            result.textContent = finalResult(playRoundResult, playerChoiceText, computerChoiceText);
+
+            const computerScoreM = document.querySelector('#computer-score');
+            computerScoreM.textContent = `Computer result: ${computerScore}`; 
+
+            const playerScoreM = document.querySelector('#player-score');
+            playerScoreM.textContent = `Player Score: ${playerScore}`
+        }
     
-    let playerChoiceText = playerChoiceToWords(playerChoice);
-    let computerChoiceText = computerChoiceToWords(computerChoice); 
 
-    let playerResult = 0;
-    let computerResult = 0;
+    let buttonSelection = document.querySelectorAll('button');
 
-    if (playerResult < 5 || computerResult < 5) {   
-        const result = document.querySelector('#final-result');
-        result.textContent = finalResult(playRoundResult, playerChoiceText, computerChoiceText);     
-        if (playRoundResult === 1) {           
-            playerResult += 1;
-        }else if (playRoundResult === 0){            
-            computerResult += 1;
-            
-        }else { } 
-
-        const playerScore = document.querySelector('#player-score');
-        playerScore.textContent = `player result:  ${playerResult}`;
-
-        const computerScore = document.querySelector('#computer-score');
-        computerScore.textContent = `Computer result: ${computerResult}`;
-        
-    }else{
-        const result = document.querySelector('#final-result');
-        result.textContent = ("End of Match");
-    }   
-    
-}
-
-let buttonSelection = document.querySelectorAll('button');
-
-buttonSelection.forEach((button) => {
-    button.addEventListener('click', () => {
-        handleButtonClick(button);
+    buttonSelection.forEach((button) => {
+        button.addEventListener('click', () => {
+            handleButtonClick(button);
+        });
     });
-});
 
-function getComputerChoice() {
+    function getComputerChoice() {
 
-    return Math.floor(Math.random() * 3);
-    
-}
+        return Math.floor(Math.random() * 3);
+        
+    }
 
-function computerChoiceToWords(q) {
-    let computerToWords;
-    if (q === 0) {
-        computerToWords = "Rock";
-    }else if (q === 1) {
-        computerToWords = "Paper";
-    }else {computerToWords = "Scissors"}
+    function computerChoiceToWords(q) {
+        let computerToWords;
+        if (q === 0) {
+            computerToWords = "Rock";
+        }else if (q === 1) {
+            computerToWords = "Paper";
+        }else {computerToWords = "Scissors"}
 
-    return computerToWords;
-}
+        return computerToWords;
+    }
 
-function playerChoiceToWords(b) {
-    let playerChoiceToWords;
-    if (b === 0) {
-        playerChoiceToWords = "Rock";
-    }else if (b === 1) {
-        playerChoiceToWords = "Paper";
-    }else {playerChoiceToWords = "Sissors"}
+    function playerChoiceToWords(b) {
+        let playerChoiceToWords;
+        if (b === 0) {
+            playerChoiceToWords = "Rock";
+        }else if (b === 1) {
+            playerChoiceToWords = "Paper";
+        }else {playerChoiceToWords = "Sissors"}
 
-    return playerChoiceToWords;
-}
+        return playerChoiceToWords;
+    }
 
-function playRound(a, b) {
-    const gameArray =[[-1,0,1],[1,-1,0],[0,1,-1]];
-    return gameArray[a][b];
-}
+    function playRound(a, b) {
+        const gameArray =[[-1,0,1],[1,-1,0],[0,1,-1]];
+        return gameArray[a][b];
+    }
 
 function finalResult(x, y, z) {
     let finalResult
@@ -87,3 +80,7 @@ function finalResult(x, y, z) {
         finalResult = `Yoy Win! ${y} beats ${z}`}
     return finalResult
 }
+}
+
+const result = document.querySelector('#final-result');
+result.textContent = "GAME FINISHED"
